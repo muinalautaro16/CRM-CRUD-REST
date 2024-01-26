d1a6b4c98cc0137b4dc2ed10d1c516c65294083c
Lautaro Muiña <87960189+muinalautaro16@users.noreply.github.com>
2024-01-26 15:34:33 -0300

    + Logica para editar cliente . + Proyecto finalizado.

--- js/API.js
+++ js/API.js
@@ -38,3 +38,30 @@ export const eliminarCliente = async id => {
         console.log(error)
     }
 }
+
+// Obtiene un cliente por su id
+export const obtenerCliente = async id => {
+    try {
+        const resultado = await fetch(`${url}/${id}`);
+        const cliente = await resultado.json();
+        return cliente;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Actualiza un registro
+export const acualizarCliente = async cliente => {
+    try {
+        fetch(`${url}/${cliente.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(cliente),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error);
+    }
+}
